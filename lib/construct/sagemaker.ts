d89849debec490efc8c54134dca15c4fb6b4f628
EC2 Default User <ec2-user@ip-172-31-45-157.eu-west-1.compute.internal>
2023-10-06 09:11:07 +0000

    Removed unnecessary parts

--- lib/construct/sagemaker.ts
+++ lib/construct/sagemaker.ts
@@ -42,30 +42,6 @@ export class SageMakerConstruct extends Construct {
           resultPath: '$.createdModel' 
         });
         
-        // Create a Step Functions task for creating a batch job in SageMaker using our pre-created model
-        // this.createTransformJob = new sfn_tasks.SageMakerCreateTransformJob(this, `${props.taskName}-Transform-Job-Task`, {
-        //   transformJobName: sfn.JsonPath.stringAt('$.BestCandidate.CandidateName'),
-        //   modelName: sfn.JsonPath.stringAt('$.BestCandidate.CandidateName'),
-        //   role: smRole,
-        //   transformInput: {
-        //     transformDataSource: {
-        //       s3DataSource: {
-        //         s3Uri: `s3://${resourceBucketName}/input/training_data.csv`,
-        //         s3DataType: sfn_tasks.S3DataType.S3_PREFIX,
-        //       }
-        //     }
-        //   },
-        //   transformOutput: {
-        //     s3OutputPath: `s3://${resourceBucketName}/output-forecasted-data`,
-        //   },
-        //   transformResources: {
-        //     instanceCount: 1,
-        //     instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE2),
-        //   },
-        //   resultPath: '$.createdJob',
-        //   maxPayload: Size.mebibytes(50)
-        // });
-        
     }
     
 }
